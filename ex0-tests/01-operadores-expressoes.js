491ff1ef27384e97a453baef9d4a58f79aafd357
Humberto Teixeira <humberto.quim@gmail.com>
2023-02-01 21:08:15 -0300

    Atualizações do dia 01.02.2023

---
+++ ex0-tests/01-operadores-expressoes.js
@@ -0,0 +1,21 @@
+console.log('OPERADOR IGUALDADE');
+console.log(1 == '1');
+
+console.log('');
+console.log('OPERADOR IGUALDADE RESTRITA');
+console.log(1 === '1');
+
+console.log('');
+console.log('COMPARAÇÃO');
+console.log(1 >= '1');
+
+console.log('');
+console.log('OPERADOR "IN"');
+var number = [0, 5, 7, 9]
+console.log(4 in number);
+
+console.log('');
+console.log('OPERADOR "INSTANCEOF"');
+var number = [0, 5, 7, 9]
+console.log(number instanceof Array);
+console.log(number instanceof String);
--- ex0-tests/script.js
+++
@@ -1,9 +0,0 @@
-var nome = 'Humberto Teixeira';
-console.log(nome);
-console.log('');
-
-var arrai = [1, 3, 5, 7];
-console.log(arrai[3]);
-console.log('');
-
-console.log('3'*'5');
