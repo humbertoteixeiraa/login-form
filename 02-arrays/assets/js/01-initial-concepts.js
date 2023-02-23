1add2a0db12e4b687b40cbfe957255e40fe146ab
Humberto Teixeira <humberto.quim@gmail.com>
2023-02-23 20:21:40 -0300

    Atualizações do dia 23.02.2023

--- 02-arrays/assets/js/01-initial-concepts.js
+++ 02-arrays/assets/js/01-initial-concepts.js
@@ -1,19 +1,24 @@
-// --> CRIANDO ARRAYS <--
-//01 - Criação de um ARRAY LITERAL
+
+console.log('--> CRIANDO ARRAYS <--');
+
+console.log('01 - Criação de um ARRAY LITERAL');
+
 var data = ['Humberto', 32, '94 kg']
 console.log(data);
 
 console.log('');
 
 
-//02 - Criação de um ARRAY com a função construtora 'new'
+console.log('02 - Criação de um ARRAY com a função construtora "new"');
+
 var data = new Array(3, 2, 1, 'testing', 1, 2, 3);
 console.log(data);
 
 console.log('');
 
 
-// --> LEITURA E GRAVAÇÃO DE ELEMENTOS EM UM ARRAY <--
+console.log('--> LEITURA E GRAVAÇÃO DE ELEMENTOS EM UM ARRAY <--');
+
 var letters = ['a', 'b', 'c', 'd', 'f']; //Gravação
 
 console.log(letters[4]); //Leitura
@@ -25,14 +30,14 @@ console.log(letters); //Leitura
 console.log('');
 
 
-// --> LEITURA DO TAMANHO DE UM ARRAY <--
+console.log('--> LEITURA DO TAMANHO DE UM ARRAY <--');
 console.log(letters.length);
 
 console.log('');
 
 
-// --> ADICIONANDO ELEMENTOS EM UM ARRAY <--
-//1ª forma:
+console.log('--> ADICIONANDO ELEMENTOS EM UM ARRAY <--');
+console.log('1ª forma:');
 test = [];
 test[0] = 'zero';
 test[2] = 'dois';
@@ -42,7 +47,8 @@ console.log(test);
 
 console.log('');
 
-//2ª forma: com o "método push()", o qual sempre insere um valor no final do ARRAY
+
+console.log('2ª forma: com o "método push()", o qual sempre insere um valor no final do ARRAY');
 test2 = [];
 test2.push(0, 1);
 test2.push('dois', 'três');
@@ -52,7 +58,7 @@ console.log(test2);
 console.log('');
 
 
-//3ª forma: com o "método unshift()", o qual sempre insere um valor no início do ARRAY
+console.log('3ª forma: com o "método unshift()", o qual sempre insere um valor no início do ARRAY');
 test2.unshift('- dois', '- um');
 
 console.log(test2);
@@ -60,17 +66,18 @@ console.log(test2);
 console.log('');
 
 
-// --> EXCLUINDO ELEMENTOS DE UM ARRAY <--
-console.log(test2);
+console.log('--> EXCLUINDO ELEMENTOS DE UM ARRAY <--');
+console.log('Antes: ', test2);
 delete test2[1]
 delete test2[2]
 delete test2[3]
-console.log(test2);
+console.log('Depois: ', test2);
 
 console.log('');
 
 
-// --> ITERAÇÃO EM ARRAYS <--
+console.log('--> ITERAÇÃO EM ARRAYS <--');
+console.log('01 - Array básico');
 var number = 10;
 var valuesArray = []; //ARRAY vazio
 
---
+++ 02-arrays/assets/js/02-metodos-arrays.js
@@ -0,0 +1,55 @@
+console.log('--> MÉTODOS DE ARRAY <--');
+console.log('01 - join()');
+
+var array1 = ['a', 'b', 1, 2];
+
+console.log('Antes: ', array1);
+console.log('Depois: ', array1.join());
+
+
+console.log('');
+
+
+console.log('02 - reverse()');
+
+var array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log('Antes: ', array2);
+console.log('Depois: ', array2.reverse());
+
+console.log('');
+
+
+console.log('03 - sort()');
+
+var array3 = ['banana', 'cajú', 'melão', 'tamarindo', 'abacate', 'limão', 'maçã', 'uva', 'pitomba', 'siriguela'];
+
+console.log('Antes: ', array3);
+console.log('Depois: ', array3.sort());
+
+console.log('');
+
+
+
+console.log('04 - concat()');
+
+var array4 = ['a', 'b', 'c', 'd', 'e'];
+
+console.log('Antes: ', array4);
+console.log('Depois: ', array4.concat(1, 2, 3, 4, 5));
+
+console.log('');
+
+
+
+console.log('05 - slice()');
+
+var array5 = ['avião', 'barco', 'carro', 'dirigível', 'elevador'];
+
+console.log('Antes: ', array5);
+console.log('Depois: ', array5.slice(0, 3));
+
+console.log('');
+
+
+
