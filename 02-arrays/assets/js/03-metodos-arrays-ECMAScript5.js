0ea5501bef44e81606f9caf4b22b310ef9464bfb
Humberto Teixeira <humberto.quim@gmail.com>
2023-02-24 11:57:08 -0300

    Atualizações do dia 24.02.2023

---
+++ 02-arrays/assets/js/03-metodos-arrays-ECMAScript5.js
@@ -0,0 +1,40 @@
+console.log('--> MÉTODOS DE ARRAY <--');
+
+console.log('01 - forEach()');
+
+var array10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log('Antes: ', array10);
+
+//Função
+function verificaPares(elemento){
+    if (elemento % 2 == 0) 
+    console.log(elemento);
+}
+
+console.log('Depois: mostrando os números pares com a função verificaPares() dentro do método forEach()');
+console.log(array10.forEach(verificaPares));
+
+
+console.log('');
+
+
+console.log('01 - forEach()');
+
+var array10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log('Antes: ', array10);
+
+//Função
+function verificaPares(elemento){
+    if (elemento % 2 == 0) 
+    console.log(elemento);
+}
+
+console.log('Depois: mostrando os números pares com a função verificaPares() dentro do método forEach()');
+console.log(array10.forEach(verificaPares));
+
+
+console.log('');
+
+
