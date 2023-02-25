a8842cf65c0fa399f57f4b0c51f05e5402db1c8d
Humberto Teixeira <humberto.quim@gmail.com>
2023-02-25 18:21:11 -0300

    Atualizações do dia 25.02.2023

---
+++ 03-functions/assets/js/02-chamando-funcoes.js
@@ -0,0 +1,13 @@
+console.log('--> CHAMANDO FUNÇÕES <--');
+
+console.log('01 - Criação de uma FUNÇÃO "comum":');
+
+function dobro (num) {
+    return num * 2;
+}
+
+console.log('Resultado: ', dobro(25));
+
+console.log('');
+//
+//
