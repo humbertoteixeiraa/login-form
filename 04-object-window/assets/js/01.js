fef9ef12a4ef91466bd836c5d9486119d3e08f9b
Humberto Teixeira <humberto.quim@gmail.com>
2023-03-09 15:50:10 -0300

    Updates of the day 03.09.2023

---
+++ 04-object-window/assets/js/01.js
@@ -0,0 +1,22 @@
+console.log('--> OBJECT WINDOW <--');
+
+console.log('01 - :');
+
+
+
+
+
+console.log('Result: ', );
+
+console.log('');
+//
+//
+
+
+
+('------------------------------------------------------------');
+console.log('');
+
+
+
+
