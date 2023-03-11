b77e12a06599233d021eb16189007dcea8d880a9
Humberto Teixeira <humberto.quim@gmail.com>
2023-03-10 21:57:33 -0300

    Updates of the day 03.10.2023

---
+++ 04-object-window/assets/js/01-concepts.js
@@ -0,0 +1,64 @@
+console.log('--> OBJECT WINDOW <--');
+
+console.log('01 - METHODS setTimeout() and setIterval():');
+
+function theAlert1() {
+    alert('Testing setTimeout() ...')
+}
+
+function theAlert2() {
+    alert('Testing setIterval() ...')
+}
+
+/*
+window.setTimeout(theAlert1, 5000);
+window.setInterval(theAlert2, 3000);
+*/
+
+
+console.log('');
+
+//
+//
+console.log('02 - OBJECT navigator:');
+
+console.log(' - Result: ', window.navigator.appName);
+console.log(' - Result: ', window.navigator.appVersion);
+console.log(' - Result: ', window.navigator.userAgent);
+console.log(' - Result: ', window.navigator.platform);
+console.log(' - Result: ', window.navigator.onLine);
+console.log(' - Result: ', window.navigator.geolocation);
+console.log(' - Result: ', window.navigator.javaEnabled);
+console.log(' - Result: ', window.navigator.cookieEnabled);
+
+console.log('');
+//
+//
+console.log('03 - OBJECT screen:');
+
+console.log(' - Result: ', window.screen.width);
+console.log(' - Result: ', window.screen.height);
+console.log(' - Result: ', window.screen.availWidth);
+console.log(' - Result: ', window.screen.availHeight);
+console.log(' - Result: ', window.screen.colorDepth);
+
+console.log('');
+//
+//
+console.log('03 - Dialog Boxes:');
+
+/*console.log(' - Result: ', window.prompt('Whats is your name ?'))*/
+/*window.alert('Testing ...')*/
+
+console.log('');
+//
+//
+
+
+
+('------------------------------------------------------------');
+console.log('');
+
+
+
+
